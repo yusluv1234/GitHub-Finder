ccaf48d5f7d5f6dcdcbafa0d09a6806bb054ef94
yusluv1234 <yusluv124@gmail.com>
2021-05-11 02:03:26 +0100

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'data_service.dart';
+import 'models.dart';
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _cityTextController = TextEditingController();
+  final _dataService = DataService();
+
+  WeatherResponse _response;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Good mporning my neighbour'),
-    );
+        home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_response != null)
+              Column(
+                children: [
+                  Image.network(_response.iconUrl),
+                  Text(
+                    '${_response.tempInfo.temperature}°',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  Text(_response.weatherInfo.description)
+                ],
+              ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: SizedBox(
+                width: 150,
+                child: TextField(
+                    controller: _cityTextController,
+                    decoration: InputDecoration(labelText: 'City'),
+                    textAlign: TextAlign.center),
+              ),
+            ),
+            ElevatedButton(onPressed: _search, child: Text('Search'))
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    setState(() => _response = response);
   }
 }
